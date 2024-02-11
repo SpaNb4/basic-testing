c78b346839db754afef5515573e05c14899ca25c
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-02-11 11:38:18 +0300

    add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,9 +1,12 @@
 import { simpleCalculator, Action } from './index';
 const testCases = [
-  { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
-  // Add more test cases for other actions
+  { a: 5, b: 3, action: Action.Add, expected: 8 },
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 5, b: 3, action: Action.Multiply, expected: 15 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 5, b: 3, action: 'InvalidAction', expected: null },
+  { a: 'InvalidNumber', b: 3, action: Action.Add, expected: null },
 ];
 
 describe('simpleCalculator', () => {
